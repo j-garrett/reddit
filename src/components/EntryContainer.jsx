349b76c1e8174aa51719d9d155d1257360a5b1ba
jon <jonathan.garrett@gmail.com>
2017-02-01 19:08:43 -0800

    Add CSS for reddit post entries

--- src/components/EntryContainer.jsx
+++ src/components/EntryContainer.jsx
@@ -4,12 +4,16 @@ import Thumbnail from './Thumbnail.jsx';
 
 const Entry = ({ title, linkToStory, thumbnail, author, clickedBoolean, subreddit, score, numOfComments, commentsUrl }) => (
   <div className="entry">
-    <h3><a href={linkToStory}>{title}</a></h3>
-    <div className="entry-score">{score}</div>
-    <Thumbnail thumbnail={thumbnail} title={title} />
-    <p className="entry-detail-text">
-      submitted to <a href={'http://www.reddit.com/r/' + subreddit}>{subreddit}</a> by <a href={'http://www.reddit.com/user/' + author}>{author}</a>. <a href={commentsUrl}>({numOfComments} comments)</a>
-    </p>
+    <div className="entry-image-thumbnail">
+      <Thumbnail thumbnail={thumbnail} title={title} />
+    </div>
+    <div className="entry-data-display">
+      <h3><a href={linkToStory}>{title}</a></h3>
+      <div className="entry-score">{score} karma</div>
+      <p className="entry-detail-text">
+        submitted to <a href={'http://www.reddit.com/r/' + subreddit}>{subreddit}</a> by <a href={'http://www.reddit.com/user/' + author}>{author}</a>. <a href={commentsUrl}>({numOfComments} comments)</a>
+      </p>
+    </div>
   </div>
 );
 
